5274c48b592bc33921444eccb3745e25266ad3c3
afedorenko <a.fedorenko@egartech.com>
2020-06-24 16:31:50 +0400

    Section 6. Lesson 18. JUnit Extensions [ВЫПОЛНЕНО НЕ ДО КОНЦА]

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestInstance;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS )
 @Tag("controllers")
 public interface ControllerTests {
 
